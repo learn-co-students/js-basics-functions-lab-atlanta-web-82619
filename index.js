1c98c5d7b5c4e7ce8eec385700ea5504c2ab1bcc
Mark Del Mar <mark@1000martians.com>
2019-10-06 14:45:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return Math.abs((block - 42) * 264);
+}
+
+function distanceTravelledInFeet(start, finish) {
+  return Math.abs((start - finish) * 264);
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) < 400){
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) > 2500){
+        return 'cannot travel that far'
+    } else if (distanceTravelledInFeet(start, destination) > 2000){
+        return 25
+    } else {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    }
+}
